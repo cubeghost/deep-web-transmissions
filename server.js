1acbca48ca66cb959cfca37d24efebc44116271f
Glitch (hello-express) <none>
2020-05-09 21:32:17 +0000

    🍮👧 Checkpoint ./views/index.html:261358/67 ./views/index.html.handlebars:261358/1253 ./views/partials/theLastDeck.html.handlebars:261358/66 ./package.json:261358/1505 ./server.js:261358/1480 ./views/index.handlebars:261358/21 ./views/partials/theLastDeck.handlebars:261358/39

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const exphbs  = require('express-handlebars');
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.get('/', (req, res) => {
+  res.render('index', {
+    timestamp: Date.now(),
+    entries: [
+      {
+        partial: 'theLastDeck',
+        title: 
+      }
+    ]
+  });
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
