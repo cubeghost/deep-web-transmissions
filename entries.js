5268e9ac621b1e51a3396643f33a660c40015048
Glitch (hello-express) <none>
2022-01-05 00:34:35 +0000

    🌄🎾 Checkpoint ./entries.js:261358/4449

--- entries.js
+++ entries.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const Twitter = require('twitter');
-const LocalCache = require('node-localcache');
+const fs = require("fs");
+const Twitter = require("twitter");
+const LocalCache = require("node-localcache");
 
-const cache = LocalCache('tmp/.cache.json', false);
+const cache = LocalCache("tmp/.cache.json", false);
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -13,77 +13,81 @@ const client = new Twitter({
 
 const sources = [
   {
-    screenName: 'tiny_star_field',
-    partialName: 'tinyStarField',
+    screenName: "tiny_star_field",
+    partialName: "tinyStarField",
     defaultEnabled: true,
   },
   {
-    screenName: 'spacetravelbot',
-    partialName: 'spaceTraveler',
+    screenName: "spacetravelbot",
+    partialName: "spaceTraveler",
     defaultEnabled: true,
   },
   {
-    screenName: 'thelastdeck',
-    partialName: 'theLastDeck',
+    screenName: "thelastdeck",
+    partialName: "theLastDeck",
     // title: 'The Pictorial Bot To The Tarot',
     defaultEnabled: true,
   },
   {
-    screenName: 'str_voyage',
-    partialName: 'aStrangeVoyage',
+    screenName: "str_voyage",
+    partialName: "aStrangeVoyage",
   },
   {
-    screenName: 'SmoothUnicode',
-    partialName: 'smoothUnicode',
+    screenName: "SmoothUnicode",
+    partialName: "smoothUnicode",
   },
   {
-    screenName: 'infinitedeserts',
-    partialName: 'infiniteDeserts',
+    screenName: "infinitedeserts",
+    partialName: "infiniteDeserts",
   },
   {
-    screenName: 'phantomfunhouse',
-    partialName: 'phantomFunhouse',
+    screenName: "phantomfunhouse",
+    partialName: "phantomFunhouse",
   },
   {
-    screenName: '10_print_chr',
-    partialName: 'tenPrintChr',
+    screenName: "10_print_chr",
+    partialName: "tenPrintChr",
     defaultEnabled: true,
   },
   {
-    screenName: 'pomological',
-    partialName: 'fruitPictures',
+    screenName: "pomological",
+    partialName: "fruitPictures",
   },
   {
-    screenName: 'boschbot',
-    partialName: 'boschBot',
+    screenName: "boschbot",
+    partialName: "boschBot",
   },
 ];
 
 module.exports = async function getEntries() {
-  const cachedEntries = cache.getItem('entries');
-  const cacheTime = cache.getItem('timestamp');
-  if (cachedEntries && (Date.now() - cacheTime) < (60 * 1000)) {
-    console.log('entries:cache:hit');
+  const cachedEntries = cache.getItem("entries");
+  const cacheTime = cache.getItem("timestamp");
+  if (cachedEntries && Date.now() - cacheTime < 60 * 1000) {
+    console.log("entries:cache:hit");
     return cachedEntries;
   }
-  
-  const entries = await Promise.all(sources.map((source) => (
-    client.get(
-      'statuses/user_timeline',
-      {screen_name: source.screenName, count: 1},
-    ).then((result) => {
-      return ({
-        partialName: source.partialName,
-        handle: source.screenName,
-        title: source.title || result[0].user.name,
-        tweet: result[0],
-        defaultEnabled: source.defaultEnabled || false,
-      });
-    })
-  )));
-  
-  console.log('entries:cache:miss');
-  cache.setItem('entries', entries);
-  cache.setItem('timestamp', Date.now());
+
+  const entries = await Promise.all(
+    sources.map((source) =>
+      client
+        .get("statuses/user_timeline", {
+          screen_name: source.screenName,
+          count: 1,
+        })
+        .then((result) => {
+          return {
+            partialName: source.partialName,
+            handle: source.screenName,
+            title: source.title || result[0].user.name,
+            tweet: result[0],
+            defaultEnabled: source.defaultEnabled || false,
+          };
+        })
+    )
+  );
+
+  console.log("entries:cache:miss");
+  cache.setItem("entries", entries);
+  cache.setItem("timestamp", Date.now());
   return entries;
-}
+};
