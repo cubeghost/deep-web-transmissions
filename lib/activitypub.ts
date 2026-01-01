b2cc864fb0df5164964844c040030cc689db19d6
alex baldwin <ghostbaldwin@gmail.com>
2025-12-31 19:59:59 -0500

    manual bsky api for now, fix type import loop

--- lib/activitypub.ts
+++ lib/activitypub.ts
@@ -6,7 +6,7 @@ import type {
   APObject,
   APActivity,
 } from "activitypub-types";
-import { SocialSource } from "./entries.ts";
+import type { SocialSource } from "./entries.ts";
 
 type ActivityPubSource = SocialSource & { type: "activitypub" };
 
--- lib/bluesky.ts
+++ lib/bluesky.ts
@@ -1,25 +1,30 @@
-import atproto from "@atproto/api";
-const { AtpAgent } = atproto;
-// console.log(Object.getOwnPropertyNames(atproto));
-console.log(AtpAgent);
-// import { Agent } from "@atproto/api";
-// console.log(Agent);
+// import { AtpAgent } from "@atproto/api";
 
-import { SocialSource } from "./entries.ts";
+import type { SocialSource } from "./entries.ts";
 
-const agent = new AtpAgent({
-  service: "https://public.api.bsky.app",
-});
+// const agent = new AtpAgent({
+//   service: "https://public.api.bsky.app",
+// });
 
 type BlueskySource = SocialSource & { type: "bluesky" };
 
+const PUBLIC_API_BASE = "https://public.api.bsky.app";
+
 export async function fetchBlueskyLatest(source: BlueskySource) {
-  const {
-    data: { feed },
-  } = await agent.getAuthorFeed({
-    actor: source.handle,
-    limit: 1,
-  });
+  // const {
+  //   data: { feed },
+  // } = await agent.getAuthorFeed({
+  //   actor: source.handle,
+  //   limit: 1,
+  // });
+
+  // @atproto/api breaks in production deno build so do it manually for now
+  const url = new URL("/xrpc/app.bsky.feed.getAuthorFeed", PUBLIC_API_BASE);
+  url.searchParams.append("actor", source.handle);
+  url.searchParams.append("limit", "1");
+  const response = await fetch(url.toString());
+
+  const feed = await response.json();
 
   return feed[0]?.post;
 }
