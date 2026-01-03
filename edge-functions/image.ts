1bcd4e278a328be3d377e29d7dd8aefaf118fd7e
alex baldwin <ghostbaldwin@gmail.com>
2026-01-03 14:17:56 -0500

    origin check on images

--- edge-functions/image.ts
+++ edge-functions/image.ts
@@ -2,6 +2,12 @@ import { getStore } from "@netlify/blobs";
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
+  if (!checkOrigin(request, context)) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   const id = context.params.id;
   const images = getStore("images");
   const blob = await images.get(id, {
@@ -21,3 +27,11 @@ export const config: Config = {
   cache: "manual",
   path: "/image/:id.png",
 };
+
+function checkOrigin(request: Request, context: Context) {
+  const referer = request.headers.get("referer");
+  if (!referer) return true;
+
+  const refererOrigin = new URL(referer).origin;
+  return refererOrigin === context.url.origin;
+}
